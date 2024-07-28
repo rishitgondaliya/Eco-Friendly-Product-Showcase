cc718bf38a3b41cdb3eaf33757d869c1ab1671f3
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2024-07-28 13:19:27 +0530

    modified

--- NaturesNook/Components/Home/Home.jsx
+++ NaturesNook/Components/Home/Home.jsx
@@ -1,6 +1,15 @@
+import img1 from '../../src/assets/item1.jpg'
+
 export default function Home(){
     return(
         <>
+            <div className="flex bg-transparent">
+                <div className="w-full h-svh">
+                    <img src={img1} alt="" />
+                    {/* <h1>hello</h1> */}
+                </div>
+                
+            </div>
         </>
     )
 }
