e7a06255fae0377bc377cf939ddae6c0748461d7
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2024-07-29 21:25:20 +0530

    added product routes

--- NaturesNook/src/App.jsx
+++ NaturesNook/src/App.jsx
@@ -1,35 +1,31 @@
 import "./App.css";
 import Layout from "../Components/Layout/Layout";
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import Home from "../Components/Home/Home";
 import About from "../Components/About/About";
 import Contact from "../Components/Contact/Contact";
-
+import Product from "../Components/Products/Product";
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Layout />,
-      children: [
-        {
-          path: "home",
-          element: <Home />,
-        },
-        {
-          path: "about",
-          element: <About />,
-        },
-        {
-          path: "contact",
-          element: <Contact />,
-        }
-      ]
-    }
-  ])
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+        <Route index={true} path="home" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="product" element={<Product />} />
+        <Route path="product/:category" element={<Product />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>
+    )
+  );   
   return (
     <>
-      <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </>
   );
 }
