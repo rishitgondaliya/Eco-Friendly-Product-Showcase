03c822caf9577dbf00923a8ccc920e531b3ad349
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2024-08-03 15:27:58 +0530

    manage paths for deploy

--- NaturesNook/vite.config.js
+++ NaturesNook/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',  // Ensure this matches the directory Netlify expects
+    outDir: 'dist',  
+    assetsDir: 'assets',
   },
 })
